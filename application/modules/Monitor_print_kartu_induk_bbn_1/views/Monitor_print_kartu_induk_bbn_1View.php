5ffe3e46a42dde1ab5978ac17382ea6b12b24772
Rezkhadhitya <darkroomeffect@gmail.com>
2019-12-18 12:14:30 +0700

    update ki

--- application/modules/Monitor_print_kartu_induk_bbn_1/views/Monitor_print_kartu_induk_bbn_1View.php
+++ application/modules/Monitor_print_kartu_induk_bbn_1/views/Monitor_print_kartu_induk_bbn_1View.php
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 <div class="page-body">
     <div class="row">
         <div class="col-sm-12">
@@ -100,3 +101,410 @@
                     <div class="col-12">
                         <button class="btn btn-inverse btn-sm btn-block">SIMPAN & CETAK</button>
                     </div>
+=======
+<div class="m-portlet mb-2">
+	<!--begin::Form-->
+
+	<div class="m-portlet__body">
+		<ul class="nav nav-tabs" role="tablist">
+			<li class="nav-item">
+				<a class="nav-link active show" data-toggle="tab" href="#" data-target="#m_tabs_1_1">Tanggal</a>
+			</li>
+			<li class="nav-item">
+				<a class="nav-link" data-toggle="tab" href="#m_tabs_1_3">Rentang</a>
+			</li>
+		</ul>
+
+		<div class="tab-content">
+			<div class="tab-pane active show" id="m_tabs_1_1" role="tabpanel">
+				<div class="form-group row">
+					<label class="col-2 col-form-label">Tanggal</label>
+					<div class="col-lg-2">
+						<input type="text" class="form-control m-input tanggal" id="CrTanggalAwal" value="26/11/2019" name="CrTanggalAwal" placeholder="Tanggal">
+					</div>
+					<label class="col-lg-2 col-form-label">Operator Entry</label>
+					<div class="col-lg-3">
+						<select class="form-control m-input" name="option">
+							<option value="">- Pilih -</option>
+							<option>1</option>
+							<option>2</option>
+							<option>3</option>
+							<option>4</option>
+							<option>5</option>
+						</select>
+					</div>
+					<a href="#" class="btn btn-brand mr-2"><i class="la la-refresh"></i></a>
+					<button type="submit" class="btn btn-brand"><i class="la la-cloud"></i> Ambil Data Server</button>
+				</div>
+			</div>
+			<div class="tab-pane" id="m_tabs_1_3" role="tabpanel">
+				<div class="form-group row">
+					<label class="col-lg-2 col-form-label">No. BPKB Awal</label>
+					<div class="col-lg-2">
+						<input type="email" class="form-control m-input" placeholder="Enter full name">
+					</div>
+
+					<label class="col-lg-2 col-form-label">No. BPKB Akhir</label>
+					<div class="col-lg-2">
+						<input type="email" class="form-control m-input" placeholder="Enter full name">
+					</div>
+					<button type="submit" class="btn btn-brand"><i class="la la-cloud"></i> Ambil Data Server</button>
+				</div>
+			</div>
+
+		</div>
+	</div>
+</div>
+
+<div class="m-portlet">
+	<div class="m-portlet__body">
+		<div class="row">
+
+			<div class="col-3">
+
+				<!--begin:: Packages-->
+				<div class="m-portlet m--bg-primary m-portlet--bordered-semi m-portlet--full-height ">
+					<div class="m-portlet__head">
+						<div class="m-portlet__head-caption">
+							<div class="m-portlet__head-title">
+								<h3 class="m-portlet__head-text m--font-light">
+									# BIRU #
+								</h3>
+							</div>
+						</div>
+					</div>
+					<div class="m-portlet__body">
+
+						<!--begin::Widget 29-->
+						<div class="m-widget29">
+							<div class="m-widget_content">
+								<h3 class="m-widget_content-title">Monthly Income</h3>
+								<div class="m-widget_content-items">
+									<div class="m-widget_content-item">
+										<span>Total</span>
+										<span class="m--font-accent">$680</span>
+									</div>
+									<div class="m-widget_content-item">
+										<span>Change</span>
+										<span class="m--font-brand">+15%</span>
+									</div>
+									<div class="m-widget_content-item">
+										<span>Licenses</span>
+										<span>29</span>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<!--end::Widget 29-->
+					</div>
+				</div>
+
+				<!--end:: Packages-->
+			</div>
+
+			<div class="col-3">
+
+				<!--begin:: Packages-->
+				<div class="m-portlet m--bg-success m-portlet--bordered-semi m-portlet--full-height ">
+					<div class="m-portlet__head">
+						<div class="m-portlet__head-caption">
+							<div class="m-portlet__head-title">
+								<h3 class="m-portlet__head-text m--font-light">
+									# HIJAU #
+								</h3>
+							</div>
+						</div>
+					</div>
+					<div class="m-portlet__body">
+
+						<!--begin::Widget 29-->
+						<div class="m-widget29">
+							<div class="m-widget_content">
+								<h3 class="m-widget_content-title">Monthly Income</h3>
+								<div class="m-widget_content-items">
+									<div class="m-widget_content-item">
+										<span>Total</span>
+										<span class="m--font-accent">$680</span>
+									</div>
+									<div class="m-widget_content-item">
+										<span>Change</span>
+										<span class="m--font-brand">+15%</span>
+									</div>
+									<div class="m-widget_content-item">
+										<span>Licenses</span>
+										<span>29</span>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<!--end::Widget 29-->
+					</div>
+				</div>
+
+				<!--end:: Packages-->
+			</div>
+
+			<div class="col-3">
+
+				<!--begin:: Packages-->
+				<div class="m-portlet m--bg-warning m-portlet--bordered-semi m-portlet--full-height ">
+					<div class="m-portlet__head">
+						<div class="m-portlet__head-caption">
+							<div class="m-portlet__head-title">
+								<h3 class="m-portlet__head-text m--font-light">
+									# KUNING #
+								</h3>
+							</div>
+						</div>
+					</div>
+					<div class="m-portlet__body">
+
+						<!--begin::Widget 29-->
+						<div class="m-widget29">
+							<div class="m-widget_content">
+								<h3 class="m-widget_content-title">Monthly Income</h3>
+								<div class="m-widget_content-items">
+									<div class="m-widget_content-item">
+										<span>Total</span>
+										<span class="m--font-accent">$680</span>
+									</div>
+									<div class="m-widget_content-item">
+										<span>Change</span>
+										<span class="m--font-brand">+15%</span>
+									</div>
+									<div class="m-widget_content-item">
+										<span>Licenses</span>
+										<span>29</span>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<!--end::Widget 29-->
+					</div>
+				</div>
+
+				<!--end:: Packages-->
+			</div>
+
+			<div class="col-3">
+
+				<!--begin:: Packages-->
+				<div class="m-portlet m--bg-danger m-portlet--bordered-semi m-portlet--full-height ">
+					<div class="m-portlet__head">
+						<div class="m-portlet__head-caption">
+							<div class="m-portlet__head-title">
+								<h3 class="m-portlet__head-text m--font-light">
+									# MERAH #
+								</h3>
+							</div>
+						</div>
+					</div>
+					<div class="m-portlet__body">
+
+						<!--begin::Widget 29-->
+						<div class="m-widget29">
+							<div class="m-widget_content">
+								<h3 class="m-widget_content-title">Monthly Income</h3>
+								<div class="m-widget_content-items">
+									<div class="m-widget_content-item">
+										<span>Total</span>
+										<span class="m--font-accent">$680</span>
+									</div>
+									<div class="m-widget_content-item">
+										<span>Change</span>
+										<span class="m--font-brand">+15%</span>
+									</div>
+									<div class="m-widget_content-item">
+										<span>Licenses</span>
+										<span>29</span>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<!--end::Widget 29-->
+					</div>
+				</div>
+
+				<!--end:: Packages-->
+			</div>
+		</div>
+	</div>
+
+	<div class="m-portlet__body">
+		<div class="row">
+			<div class="col-3">
+				<div class="card text-white bg-primary">
+					<div class=" card-header">Header</div>
+					<div class="card-body">
+						<h5 class="card-title">Primary card title</h5>
+						<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-3">
+				<div class="card text-white bg-primary">
+					<div class="card-header">Header</div>
+					<div class="card-body">
+						<h5 class="card-title">Primary card title</h5>
+						<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-3">
+				<div class="card text-white bg-primary">
+					<div class="card-header">Header</div>
+					<div class="card-body">
+						<h5 class="card-title">Primary card title</h5>
+						<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					</div>
+				</div>
+			</div>
+
+			<div class="col-3">
+				<div class="card text-white bg-primary">
+					<div class="card-header">Header</div>
+					<div class="card-body">
+						<h5 class="card-title">Primary card title</h5>
+						<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<div class="m-portlet ">
+		<div class="m-portlet__body m-portlet__body--no-padding">
+			<div class="row m-row--no-padding m-row--col-separator-xl">
+
+				<div class="col-md-12 col-lg-6 col-xl-3 m-0">
+
+					<!--begin::New Feedbacks-->
+					<div class="m-widget24 m--bg-success border border-primary">
+						<div class="m-widget24__item">
+							<h3 class="m-widget24__title">
+								# HIJAU #
+							</h3><br>
+							<span class="m-widget24__desc">
+								Customer Review
+							</span>
+							<span class="m-widget24__stats m--font-info">
+								1349
+							</span>
+							<div class="m--space-10"></div>
+							<div class="progress m-progress--sm">
+								<div class="progress-bar m--bg-info" role="progressbar" style="width: 84%;" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+							</div>
+							<span class="m-widget24__change">
+								Change
+							</span>
+							<span class="m-widget24__number">
+								84%
+							</span>
+						</div>
+					</div>
+
+					<!--end::New Feedbacks-->
+				</div>
+
+				<div class="col-md-12 col-lg-6 col-xl-3 m-0">
+
+					<!--begin::New Feedbacks-->
+					<div class="m-widget24 m--bg-success border border-primary">
+						<div class="m-widget24__item">
+							<h3 class="m-widget24__title">
+								# HIJAU #
+							</h3><br>
+							<span class="m-widget24__desc">
+								Customer Review
+							</span>
+							<span class="m-widget24__stats m--font-info">
+								1349
+							</span>
+							<div class="m--space-10"></div>
+							<div class="progress m-progress--sm">
+								<div class="progress-bar m--bg-info" role="progressbar" style="width: 84%;" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+							</div>
+							<span class="m-widget24__change">
+								Change
+							</span>
+							<span class="m-widget24__number">
+								84%
+							</span>
+						</div>
+					</div>
+
+					<!--end::New Feedbacks-->
+				</div>
+
+				<div class="col-md-12 col-lg-6 col-xl-3 m-0">
+
+					<!--begin::New Orders-->
+					<div class="m-widget24 m--bg-warning border border-primary">
+						<div class="m-widget24__item">
+							<h3 class="m-widget24__title">
+								# KUNING #
+							</h3><br>
+							<span class="m-widget24__desc">
+								Fresh Order Amount
+							</span>
+							<span class="m-widget24__stats m--font-danger">
+								567
+							</span>
+							<div class="m--space-10"></div>
+							<div class="progress m-progress--sm">
+								<div class="progress-bar m--bg-danger" role="progressbar" style="width: 69%;" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+							</div>
+							<span class="m-widget24__change">
+								Change
+							</span>
+							<span class="m-widget24__number">
+								69%
+							</span>
+						</div>
+					</div>
+
+					<!--end::New Orders-->
+				</div>
+
+				<div class="col-md-12 col-lg-6 col-xl-3 m-0">
+
+					<!--begin::New Users-->
+					<div class="m-widget24 m--bg-danger border border-primary">
+						<div class="m-widget24__item">
+							<h3 class="m-widget24__title">
+								# MERAH #
+							</h3><br>
+							<span class="m-widget24__desc">
+								Joined New User
+							</span>
+							<span class="m-widget24__stats m--font-success">
+								276
+							</span>
+							<div class="m--space-10"></div>
+							<div class="progress m-progress--sm">
+								<div class="progress-bar m--bg-success" role="progressbar" style="width: 90%;" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+							</div>
+							<span class="m-widget24__change">
+								Change
+							</span>
+							<span class="m-widget24__number">
+								90%
+							</span>
+						</div>
+					</div>
+
+					<!--end::New Users-->
+				</div>
+
+			</div>
+		</div>
+	</div>
+
+</div>
+>>>>>>> Stashed changes
