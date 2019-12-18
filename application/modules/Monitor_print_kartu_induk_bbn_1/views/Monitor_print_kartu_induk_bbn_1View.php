9e069b446dd367cac898f993eab551113bec8cae
Rezkhadhitya <darkroomeffect@gmail.com>
2019-12-18 12:37:17 +0700

    Update KI

--- application/modules/Monitor_print_kartu_induk_bbn_1/views/Monitor_print_kartu_induk_bbn_1View.php
+++ application/modules/Monitor_print_kartu_induk_bbn_1/views/Monitor_print_kartu_induk_bbn_1View.php
@@ -1,106 +1,101 @@
 <div class="page-body">
-    <div class="row">
-        <div class="col-sm-12">
-            <div class="card">
-                <div class="card-block">
-                    <!-- Begin Page Content -->
-                    <div class="row">
+	<div class="row">
+		<div class="col-sm-12">
+			<div class="card">
+				<div class="card-block">
+					<!-- Begin Page Content -->
+					<div class="row">
 
-                        <div class="col-6 ml-3">
-                            <div class="form row">
-                                <div class="col-sm-4 mb-2">
-                                    <label>
-                                        <input type="radio" name="member" value="free" data-bv-field="member">
-                                        <i class="helper"></i> TANGGAL BPKB
-                                    </label>
-                                </div>
-                            <div class="col-3">
-                                <input id="userName-2" name="userName" type="text" class="required form-control">
-                            </div>
-                            <div class="col-1">
-                                <label for="userName-2" class="block">s/d</label>
-                            </div>
-                            <div class="col-3">
-                                <input id="userName-2" name="userName" type="text" class="required form-control">
-                            </div>
-                            </div>
-                        </div>
-                    </div>
+						<div class="col-6 ml-3">
+							<div class="form row">
+								<div class="col-sm-4 mb-2">
+									<label>
+										<input type="radio" name="member" value="free" data-bv-field="member">
+										<i class="helper"></i> TANGGAL BPKB
+									</label>
+								</div>
+								<div class="col-3">
+									<input id="userName-2" name="userName" type="text" class="required form-control">
+								</div>
+								<div class="col-1">
+									<label for="userName-2" class="block">s/d</label>
+								</div>
+								<div class="col-3">
+									<input id="userName-2" name="userName" type="text" class="required form-control">
+								</div>
+							</div>
+						</div>
+					</div>
 
-                    <div class="row">
+					<div class="row">
 
-                    <div class="col-6 ml-3">
-                            <div class="form row">
-                                <div class="col-sm-4 mb-2">
-                                    <label>
-                                        <input type="radio" name="member" value="free" data-bv-field="member">
-                                        <i class="helper"></i> No BPKB
-                                    </label>
-                                </div>
-                             <div class="col-3">
-                                <input id="userName-2" name="userName" type="text" class="required form-control">
-                             </div>
-                             <div class="col-1">
-                                <label for="userName-2" class="block">s/d</label>
-                             </div>
-                             <div class="col-3">
-                                <input id="userName-2" name="userName" type="text" class="required form-control">
-                           </div>
-                          </div>
-                        </div>
-                      </div>
-                      <hr>
-                      <div class="row">
-                             <div class="col-12 ml-3">
-                                 <button class="btn btn-inverse btn-sm btn-block">CARI DATA</button>
-                             </div>
-                    </div>
-                 <!-- Nav tabs -->
+						<div class="col-6 ml-3">
+							<div class="form row">
+								<div class="col-sm-4 mb-2">
+									<label>
+										<input type="radio" name="member" value="free" data-bv-field="member">
+										<i class="helper"></i> No BPKB
+									</label>
+								</div>
+								<div class="col-3">
+									<input id="userName-2" name="userName" type="text" class="required form-control">
+								</div>
+								<div class="col-1">
+									<label for="userName-2" class="block">s/d</label>
+								</div>
+								<div class="col-3">
+									<input id="userName-2" name="userName" type="text" class="required form-control">
+								</div>
+							</div>
+						</div>
+					</div>
+					<hr>
+					<div class="row">
+						<div class="col-12 ml-3">
+							<button class="btn btn-inverse btn-sm btn-block">CARI DATA</button>
+						</div>
+					</div>
+					<!-- Nav tabs -->
 
-           <div class="card">
-             <div class="card-block">
-             <div class="dt-responsive table-responsive">
-                <table id="multi-colum-dt" class="table table-striped table-bordered nowrap">
-                    <thead>
-                        <tr>
-                            <th>No BPKB</th>
-                            <th>Alamat Pemilik</th>
-                            <th>No Polisi</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr>
-                            <td></td>
-                            <td></td>
-                            <td></td>
-                        </tr>
-                        <tr>
-                            <td>P 01234568 R</td>
-                            <td>P 01234568 R</td>
-                            <td>Tokyo</td>
-                        </tr>
-                        <tr>
-                            <td>P 01234568 R</td>
-                            <td>Junior Technical Author</td>
-                            <td>San Francisco</td>
-                        </tr>
-                        <tr>
-                            <td>P 01234568 R</td>
-                            <td>Senior Javascript Developer</td>
-                            <td>Edinburgh</td>
-                        </tr>
-                    </tbody>
-                </table>
-            </div>
-        </div>
-    </div>
-</div>
-                    <div class="row ml-3">
-                         <div class="col-12">
-                            <button class="btn btn-inverse btn-sm btn-block">SIMPAN & CETAK</button>
-                         </div>
-            
-            
-
-
- 
+					<div class="card">
+						<div class="card-block">
+							<div class="dt-responsive table-responsive">
+								<table id="multi-colum-dt" class="table table-striped table-bordered nowrap">
+									<thead>
+										<tr>
+											<th>No BPKB</th>
+											<th>Alamat Pemilik</th>
+											<th>No Polisi</th>
+										</tr>
+									</thead>
+									<tbody>
+										<tr>
+											<td></td>
+											<td></td>
+											<td></td>
+										</tr>
+										<tr>
+											<td>P 01234568 R</td>
+											<td>P 01234568 R</td>
+											<td>Tokyo</td>
+										</tr>
+										<tr>
+											<td>P 01234568 R</td>
+											<td>Junior Technical Author</td>
+											<td>San Francisco</td>
+										</tr>
+										<tr>
+											<td>P 01234568 R</td>
+											<td>Senior Javascript Developer</td>
+											<td>Edinburgh</td>
+										</tr>
+									</tbody>
+								</table>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="row ml-3">
+					<div class="col-12">
+						<button class="btn btn-inverse btn-sm btn-block">SIMPAN & CETAK</button>
+					</div>
